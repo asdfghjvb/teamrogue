a32667d657eb2c6a96b95f3a8ef49293fbf8d918
Michael M Gordon <MMGordon@student.fullsail.edu>
2024-06-11 01:18:50 -0400

    Added Player script
    Added player, floor, movement, made camera a child of player, added sprint input

---
+++ teamrogue/Assets/Scripts/Player.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+
+public class Player : MonoBehaviour
+{
+    public static Player instance;
+    [SerializeField] CharacterController playerController;
+    [SerializeField] public int health;
+    [SerializeField] public float speed;
+    [SerializeField] public float sprintMod;
+    [SerializeField] public float armorMod;
+    [SerializeField] public int jumpMax;
+    [SerializeField] int jumpSpeed;
+    [SerializeField] int gravity;
+
+    [SerializeField] public int shootDamage;
+    [SerializeField] public float shootRate;
+    [SerializeField] public int shootDist;
+
+    bool isShooting;
+    int jumpCount;
+    public int fullHealth;
+    Vector3 moveDir;
+    Vector3 playerVel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        fullHealth = health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * shootDist, Color.red);
+        Movement();
+        Sprint();
+        if (Input.GetButton("Fire1") && !isShooting)
+        {
+            StartCoroutine(shoot());
+        }
+    }
+    void Movement()
+    {
+        if (playerController.isGrounded)
+        {
+            jumpCount = 0;
+            playerVel = Vector3.zero;
+        }
+        moveDir = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
+        playerController.Move(moveDir * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpMax)
+        {
+            jumpCount++;
+            playerVel.y = jumpSpeed;
+        }
+        playerVel.y -= gravity * Time.deltaTime;
+        playerController.Move(playerVel * Time.deltaTime);
+    }
+    void Sprint()
+    {
+        if (Input.GetButtonDown("Sprint"))
+            speed *= sprintMod;
+        else if (Input.GetButtonUp("Sprint"))
+            speed /= sprintMod;
+    }
+
+    IEnumerator shoot()
+    {
+        isShooting = true;
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, shootDist))
+        {
+
+        }
+        yield return new WaitForSeconds(shootRate);
+        isShooting = false;
+    }
+
+
+}
+
