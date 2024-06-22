01162af80da0d4b1128146409e93678d894beff8
Josh Lindgren <jmlindgren@student.fullsail.edu>
2024-06-22 13:55:24 -0600

    Staff-Pickup
    added the ability to pickup new staffs in the environment, currently have 3 different staffs to pickup and swap between

---
+++ teamrogue/Assets/Scripts/Player/Bullet Player.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletPlayer : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+
+    [SerializeField] int damage;
+    [SerializeField] int speed;
+    [SerializeField] int destroyTime;
+
+    [SerializeField] bool destroyOnInpact = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //Vector3 playerPos = GameManager.instance.player.transform.position;
+        rb.velocity = (transform.forward).normalized * speed;
+        Destroy(gameObject, destroyTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        IDamage dmg = other.GetComponent<IDamage>();
+
+        if (dmg != null)
+        {
+            dmg.takeDamage(damage);
+
+            if (!destroyOnInpact) //check is here so it will still be destroyed when hitting structures
+                return;
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ teamrogue/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,202 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+
+public class Player : MonoBehaviour, IDamage
+{
+    //public static Player instance;
+    [SerializeField] CharacterController playerController;
+    [SerializeField] public float health;
+    [SerializeField] public float speed;
+    [SerializeField] public float sprintMod;
+    [SerializeField] public float armorMod;
+    [SerializeField] public int jumpMax;
+    [SerializeField] int jumpSpeed;
+    [SerializeField] int gravity;
+
+    [SerializeField] public int meleeDamage;
+    [SerializeField] float meleeRange;
+    [SerializeField] float meleeRate;
+    [SerializeField] public float meleeCooldown;
+
+
+    [SerializeField] public int shootDamage;
+    [SerializeField] public float shootRate;
+    [SerializeField] public int shootDist;
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform shootPos;
+    [SerializeField] GameObject staffModel;
+    [SerializeField] List<Staffs> staffList = new List<Staffs>();
+
+    int currentStaff;
+    bool isShooting;
+    bool isMeleeAttacking;
+    float lastMeleeTime;
+    int jumpCount;
+    public float fullHealth;
+   
+
+    Vector3 moveDir;
+    Vector3 playerVel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //instance = this;
+        fullHealth = health;
+        updatePlayerUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * shootDist, Color.red);
+        Movement();
+        Sprint();
+        UpdateMeleeCooldownUI();
+        if (Input.GetButton("Fire1") && !isShooting && staffList.Count > 0)
+        {
+            StartCoroutine(shoot());
+        }
+        selectStaff();
+
+        if (Input.GetButtonDown("Fire2") && !isMeleeAttacking && Time.time >= lastMeleeTime + meleeCooldown)
+        {
+            StartCoroutine(melee());
+        }
+    }
+    void Movement()
+    {
+        if (playerController.isGrounded)
+        {
+            jumpCount = 0;
+            playerVel = Vector3.zero;
+        }
+        moveDir = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
+        playerController.Move(moveDir * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpMax)
+        {
+            jumpCount++;
+            playerVel.y = jumpSpeed;
+        }
+        playerVel.y -= gravity * Time.deltaTime;
+        playerController.Move(playerVel * Time.deltaTime);
+    }
+    void Sprint()
+    {
+        if (Input.GetButtonDown("Sprint"))
+            speed *= sprintMod;
+        else if (Input.GetButtonUp("Sprint"))
+            speed /= sprintMod;
+    }
+
+    IEnumerator shoot()
+    {
+        //isShooting = true;
+        //RaycastHit hit;
+        //if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, shootDist))
+        //{
+        //    IDamage dmg = hit.collider.GetComponent<IDamage>();
+
+        //    if (hit.transform != transform && dmg != null)
+        //    {
+        //        dmg.takeDamage(shootDamage);
+        //    }
+        //}
+        //yield return new WaitForSeconds(shootRate);
+        //isShooting = false;
+        if (!GameManager.instance.isPaused)
+        {
+            isShooting = true;
+            Instantiate(bullet, shootPos.position, shootPos.rotation);
+
+            yield return new WaitForSeconds(shootRate);
+            isShooting = false;
+        }
+    }
+
+    IEnumerator melee() 
+    {
+        isMeleeAttacking = true;
+        lastMeleeTime = Time.time;
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position + transform.forward * meleeRange, meleeRange);
+        foreach (Collider hitCollider in hitColliders)
+        {
+            if (hitCollider.gameObject == gameObject)
+                continue;
+
+            IDamage dmg = hitCollider.GetComponent<IDamage>();
+            if (dmg != null)
+            {
+                dmg.takeDamage(meleeDamage);
+            }
+        }
+        yield return new WaitForSeconds(meleeRate);
+        isMeleeAttacking = false;
+    }
+
+    void UpdateMeleeCooldownUI()
+    {
+        float cooldownRemaining = Mathf.Clamp01((Time.time - lastMeleeTime) / meleeCooldown);
+        GameManager.instance.UpdateMeleeCooldownUI(cooldownRemaining);
+    }
+
+    void updatePlayerUI()
+    {
+        GameManager.instance.playerHealthBar.fillAmount = (float)health / fullHealth;
+    }
+
+    public void takeDamage(int amount)
+    {
+        health -= amount * armorMod;
+        updatePlayerUI();
+
+        if (health <= 0)
+        {
+            GameManager.instance.youLose();
+        }
+    }
+
+    public void getStaff(Staffs staff)
+    {
+        staffList.Add(staff);
+        currentStaff = staffList.Count - 1;
+
+
+        shootDamage = staff.staffDamage;
+        shootDist = staff.staffDistance;
+        shootRate = staff.staffSpeed;
+        bullet = staff.bullet;
+
+        staffModel.GetComponent<MeshFilter>().sharedMesh = staff.staffModel.GetComponent<MeshFilter>().sharedMesh;
+        staffModel.GetComponent<MeshRenderer>().sharedMaterial = staff.staffModel.GetComponent<MeshRenderer>().sharedMaterial;
+    }
+
+    void selectStaff()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0 && currentStaff < staffList.Count - 1)
+        {
+            currentStaff++;
+            changeStaff();
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0 && currentStaff > 0)
+        {
+            currentStaff--;
+            changeStaff();
+        }
+    }
+
+    void changeStaff()
+    {
+        shootDamage = staffList[currentStaff].staffDamage;
+        shootDist = staffList[currentStaff].staffDistance;
+        shootRate = staffList[currentStaff].staffSpeed;
+        bullet = staffList[currentStaff].bullet;
+        staffModel.GetComponent<MeshFilter>().sharedMesh = staffList[currentStaff].staffModel.GetComponent<MeshFilter>().sharedMesh;
+        staffModel.GetComponent<MeshRenderer>().sharedMaterial = staffList[currentStaff].staffModel.GetComponent<MeshRenderer>().sharedMaterial;
+    }
+}
+
